948da7f9bf3a942cf7337f8515338513908bd494
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-04 11:47:32 +0000

    add all for now

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -2,3 +2,21 @@ const accountId = 145514
 let accountEmail = "bikram@gmail.com"
 var accountPassword = "654321"
 accountCity = "lakshmi"
+let accountState;
+/*you can change or declar any variable name without calling any variable before it. But remember it is not a good practice, you need to avoid this*/
+// accountId = 112233 // not allowed
+// ctrl + / short cut for comment out
+accountEmail = "bibib@gmail.com"
+accountPassword = "876543"
+accountCity = "kharda"
+
+/*never use var keyword as it is global scoped. which slas and asterisk sign i am using ,it is use for multiline comments*/
+
+
+console.log(accountId);
+console.table([accountId,accountEmail,accountPassword,accountCity,accountState]);
+
+
+
+
+
