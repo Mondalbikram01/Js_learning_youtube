27b85e1edd6022eea43f6b8edd37524cd9574a43
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-04 12:30:31 +0000

    know datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,26 @@
+"use strict";//treat all js code as a newer version
+
+// alert(3+3) //it will not work as we are using node.js not browser
+let name = "Bikram"
+let age = 2
+let islogedin = true
+ 
+//number
+// bigint
+// string
+//boolean = true/false
+//null = empty(standalone value)
+//undefined(when we didn't put any value inside this)
+//symbol
+
+//above all are premetive detatypes
+
+//object
+//we can find datatype of any object by "type of"
+console.log(typeof age)//mumber
+console.log(typeof null)//object
+console.log(typeof undefined)//undefined
+
+
+
+//read ECMAscript documentation for more information but don't read too much
