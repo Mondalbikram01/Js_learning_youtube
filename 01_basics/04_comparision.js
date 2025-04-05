635290dcfed2c2cdaa31fa026f76c31f4d31c765
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-05 07:55:27 +0000

    done comparison

---
+++ 01_basics/04_comparision.js
@@ -0,0 +1,26 @@
+//basic comparison operator
+// console.log(1 < 2)
+// console.log(1 > 2)
+// console.log(1 <= 2)
+// console.log(1 >= 2)
+// console.log(1 == 2)
+// console.log(1 != 2)
+// this all give you the Boolean value 
+
+//datatype conparison
+// console.log("2" > 1)
+// console.log("02" > 1)
+
+// console.log(null > 0)
+// console.log(null == 0)
+// console.log(null >= 0)//reasion of this is that equality check and comparion operator are work differntly . Comparisons convert null to a number, treating it as 0. that why null >= 0 is true and null > false.
+
+//null and undefined are confusing comparison
+
+// console.log(undefined > 0)
+// console.log(undefined == 0)
+// console.log(undefined < 0)
+
+//=== it checks not only value but also detatypes
+// console.log("2" == 2)
+// console.log("2" === 2)
