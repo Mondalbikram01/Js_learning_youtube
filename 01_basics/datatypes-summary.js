f0225ecd3fb988b2ae95a81d85f9caf1557d4939
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-06 07:04:31 +0000

    end of datatype summary

--- 01_basics/datatypes-summary.js
+++ 01_basics/datatypes-summary.js
@@ -15,7 +15,22 @@ const anotherid =Symbol('123')
 // though another veriables symbol looks same they will not same. 
 console.log(id === anotherid)
 
+// const BigNumber = 1234567890877654323n
+// when you put 'n' at the  ene of a big number it become BigInt datatype 
+
 //Non-premetive datatypes
 /* Array , object , functions */
-
+// Array must be in [] bracket
+const heros = ["ironman" , "hulk" ,"captain"];
+//objects must be in curly braket with key-value pairs. you can store it inside a variable also.
+{
+   name : "Bikram"
+   age : 25
+}
+// you can create just a function by its syntax or you can create a function inside a variable. both examples ara given below
+// function(){}// syntax
+const myfunction = function(){
+    console.log("Hello World");
+}
+/*when you find datatype of any Non-premetive datatypes using typeof you get function. when you find datatype of a function using typeof you will get function which is called object function*/
 //JavaScript is dynamicaly typed language
