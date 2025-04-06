e785ae5672aafe2d30097a59b2afd2a5abab5d28
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-06 14:38:38 +0000

    pause at index of

---
+++ 01_basics/05_string.js
@@ -0,0 +1,23 @@
+const name = "Bikram"
+const repoCount = 20
+// you can concatinate using + , by this it means you can add them both
+// console.log(name + repoCount + "something")//for better code writing don't use this. 
+// use interpoletion which is given below. here use backticks `` and ${}
+console.log(`My name is ${name} and my number of repository is ${repoCount}`)
+
+// something different 
+const gameName =  new String('bikram01')
+// this way you will gate more information if you write it in browser console
+// console.log(gameName)
+
+// you can get charecter by index number of the string
+console.log(gameName[0])
+// you can access manything by this way console.log(veriable name.method)
+// console.log(gameName.length)
+// console.log(gameName.toUpperCase())
+console.log(gameName.charAt(3))//you can get charecter by index number of the string. this way also 
+console.log(gameName.indexOf('k'))//you can see the charecter position by this way. it must be inside cotetion 
+
+
+
+// use terminator is a good habit 
