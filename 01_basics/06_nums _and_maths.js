e19a40e37a95911ec7f89aeaaed6262f6f0cab65
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-07 12:38:37 +0000

    facing problem with 06 file

--- 01_basics/06_nums _and_maths.js
+++
@@ -1,6 +0,0 @@
-const score = 100
-console.log(score)
-// it is a number datatype but more code writing practice use this wriing given below
-const newScore = new Number(100)
-console.log(newScore)
-// when you use new with datatype then value will be specified for the datatype
