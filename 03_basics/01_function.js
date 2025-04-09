57b95cb3ca5d84a4de85dad575b2bb3a3e5ea1fd
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-09 11:01:18 +0000

    function syntax

---
+++ 03_basics/01_function.js
@@ -0,0 +1,12 @@
+// function is a bunch of code lines inside in a packet . you can use it anywhere in your code by calling its name
+// syntax function_keyword defination_name(){scope}
+
+function sayMyName(){
+    console.log('B');
+    console.log('I');
+    console.log('K');
+    console.log('R');
+    console.log('A');
+    console.log('M');
+}
+sayMyName()//this is the function call syntax, just sayMyName is defination and with parenthesis it will exicute
