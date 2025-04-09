84cf9b9bd88be9cbe1793c4d4fd0b8abf6bdf25d
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-09 12:28:10 +0000

    some deal

--- 03_basics/01_function.js
+++ 03_basics/01_function.js
@@ -15,8 +15,8 @@ function addTwoNumber(number1,number2){// when i create a function ,here inside 
     console.log(number1 + number2);
 }
 // call the function 
-addTwoNumber(2,3)//here 2 and 3 are called argument which goes to the paramiter 
-const result = addTwoNumber(4 ,5)//in javascript you can store it inside a variable but not in this way . 
+// addTwoNumber(2,3)//here 2 and 3 are called argument which goes to the paramiter 
+// const result = addTwoNumber(4 ,5)//in javascript you can store it inside a variable but not in this way . 
 // console.log("result:", result);//here you get undefined
 
 /*console.log just print the value. but it dosn't return the value to there from where the function was called. to get the value on that variable and store it inside the variable ,use return key word. given below */
@@ -24,6 +24,41 @@ const result = addTwoNumber(4 ,5)//in javascript you can store it inside a varia
 function addTwoNumber_1(number1 , number2){
     let result = number1 + number2
     return result
+    console.log("bikram");//no code will be executed after the return keyword. RULL
 }
 const new_result = addTwoNumber_1(8 , 9)
-console.log ("new_result:", new_result);
+// console.log ("new_result:", new_result);
+
+// another way 
+function addTwoNumber_2(number1 , number2){
+    return  number1 + number2//it will return number1 + number2 without storing it
+}
+const another_result = addTwoNumber_2(7 , 8)
+// console.log ("another_result:", another_result);
+
+
+
+function loginUserMessage(username){
+    return `${username} just logged in`
+}
+// const status = loginUserMessage("Bikram")
+// console.log(status);//you can do this or do the given below
+// console.log(loginUserMessage("Bikram"));// it will print directly //go with this
+
+// suppose user doesn't put anything then the value will be undefined or Null. to avoid this do ..
+function loginUserMessage_1(username = "empty"){//set a default value
+    return `${username} just logged in`
+}
+// console.log(loginUserMessage_1())
+
+// best way is ..using if else
+function loginUserMessage_2(username){
+    if (!username){//here is the concept is that username is false(as nothing is inside) , by using ! it will become true and then scope start.
+        console.log("pls enter an username");
+        return
+    }
+    else{
+        return `${username} just logged in`
+    }
+}
+console.log(loginUserMessage_2("Bikram"));
