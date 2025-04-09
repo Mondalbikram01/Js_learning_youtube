59f16803f6650b6dce9510871a5cd2e743fa17c3
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-09 16:30:33 +0000

    basic scope

---
+++ 03_basics/02_scope.js
@@ -0,0 +1,8 @@
+// {} braces is called scope but it is also used in object making
+// inside the braces is called block scope and outside the braces is called global scope
+// var c = 4;//we don't use it as var is global scope
+if (true){
+    let a = 2
+    const b = 3
+}
+// browser and code enviroment, both  global scope are different
