bbf1aac45aaec37c93194ea78a47e1a46355770c
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-09 17:17:57 +0000

    scope and mini hosting

--- 03_basics/02_scope.js
+++ 03_basics/02_scope.js
@@ -6,3 +6,42 @@ if (true){
     const b = 3
 }
 // browser and code enviroment, both  global scope are different
+
+
+// nested function
+// shot trick to remember : junior can get from senior but senior can't get from junior.
+function one(){
+    const username = "Bikram"
+    function two(){
+        const website = "youtube"
+        console.log(username);
+
+    }
+    // console.log(website);//you can't get it from here(don't uncomment)
+    two()
+}
+// one()
+
+if (true){
+    const username = "Bikram"
+    if (username === "Bikram"){
+        const website = " youtube"
+        // console.log(username + website);
+    }
+}
+
+
+// +++++++++++++++++++++++++++++++++++++++++++++++interesting+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+console.log(addOne(5));//with addTwo can't do this
+function addOne(num){
+    return num + 1
+}
+
+// an expression example
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(9));//this is called expression, here variable holds function
+/*IMPORTANT : you can call addOne before function but you can't call addTwo before the declaration. the example above .
+This is the concept of hosting, we will learn it leter.*/
