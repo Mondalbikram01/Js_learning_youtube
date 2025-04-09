56a18de8339a8beb98f0db54afadd9ed9bf54100
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-09 03:43:13 +0000

    arrays done for now

--- 02_basics/02_array.js
+++ 02_basics/02_array.js
@@ -9,5 +9,23 @@ const allHeros = marvel_heros.concat(dc_heros)//it add all elements in one array
 
 // there is an easy way of concat
 const all_new_heros = [...marvel_heros,...dc_heros]//its called spread
-console.log(all_new_heros);//it do the same as concat
+// console.log(all_new_heros);//it do the same as concat
+
+const another_array = [1, 2, 3, [4, 5, 6] , 7 , [6, 7, [4, 5]]]
+// inside this there is array inside array. but we want all elements in one array
+// use flat and give the depth inside parenthesis
+const real_another_array = another_array.flat(Infinity)//it doesn't change actual array, it gives you a new array
+// console.log(real_another_array);
+// console.log(another_array);
+
+console.log(Array.isArray("Bikram"));//here i am checking that is it array or not an array inside the parenthesis. if it isn't then will change the  way given below
+console.log(Array.from("Bikram"));//it change to an array
+console.log(Array.from({ name : "Bikram"}));//javaScript can't convert this into an array and give you a empty array. interesting
+
+let score1 = 100
+let score2 = 200
+let score3 = 300 
+// make an array with all this variables
+console.log(Array.of(score1, score2, score3));//returns an array from a set of elements
+
 
