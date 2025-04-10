91437dde2633ab0e9cdf5ae4acee52c4328c72ff
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-10 17:12:50 +0000

    for loop

---
+++ 05_iteration/one.js
@@ -0,0 +1,40 @@
+// for loop
+/*syntax in my language : first inside the parenthesis you declare a valiable name index and then after it you will find a condition with array. next there Will be a increment and at last parenthesis . The flow go like this .. first variable check then condition and flow go inside the parenthesis and then come to increment , next again go the flow inside code*/
+// for (let index = 0; index < array.length; index++) {
+//     const element = array[index];
+    
+// }
+
+
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;//for first time index 0 comes here and then everytime come index++ value
+//     console.log(element);
+    
+// }
+// // ctrl + D is the short cut key to access all same keywords, first double click and then shortcut key
+
+// a condition inside loop
+// for (let i = 0; i <= 10; i++) {
+//     const element = i;
+//     if (element == 5){
+//         console.log("5 is best");
+//     }
+//     console.log(element);
+    
+// }
+
+
+// loop inside loop
+// for (let i = 0; i <= 10; i++) {
+//     console.log(`Outer loop ${i}`)
+//     for (let j = 0; j <= 10; j++) {
+//         console.log(`inner loop ${j} and on going outer loop ${i}`)
+        
+//     }
+    
+// }
+// here outer loop 1 time = inner loop 10 times
+
+for (let i = 0; i <= 10; i++) {
+        console.log(i);//no need to store any where
+}
