9c82fef0d61acfe931fb5b8dfab429fefd858d23
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-10 06:31:31 +0000

    arrow function done

--- 03_basics/03_arrow.js
+++ 03_basics/03_arrow.js
@@ -7,9 +7,9 @@ const user = {
     }
 }
 
-user.welcomeMessage()//it will print the default value
-user.username = "sam"//now i change the default value
-user.welcomeMessage()//it will print the recent value
+// user.welcomeMessage()//it will print the default value
+// user.username = "sam"//now i change the default value
+// user.welcomeMessage()//it will print the recent value
 
 // console.log(this)
 /*as we inside the node enviroment , this keyword give you empty object. but when you run this keyword you will get window object.*/
@@ -26,3 +26,42 @@ function chai_2(){
     console.log(this.username);//you can't do it as it is just a function . you need to be inside the object
 }
 // chai_2()//undefined
+
+const chai_3 = function (){
+    let username = "Bikram"
+    console.log(this.username);//you can't do it as it is just a function though it is stored in a variable . you need to be inside the object
+}
+// chai_3()//though you store it in a variable you get undefined result
+
+
+// arrow function
+// remove the function keyword and add => after parenthesis
+const arrowFunc = () => {
+    let username = "Bikram"
+    console.log(this.username);//this will also give you undefined result
+    console.log(this);//this will also give you empty object result
+}
+// arrowFunc()
+
+// arrow function syntax () => {}
+// you can store it inside a variable like const addTwo = () => {} . and everything is same with function
+
+//basic arrow function
+// const addTwo = (num1 , num2) => {
+//     console.log(num1 + num2);//you can do this also using return keyword
+// }
+// addTwo(2,3)//when you use return need to use console.log
+
+//**IMPORTANT : implicit return//basically it is arrow function
+// no need to use curly braces. also you can remove return keyword
+const addTwo = (num1 , num2) => (num1 + num2)
+
+// console.log(addTwo(2,3));
+//when you use curly braces you need to write return keyword and if you use parenthesis you don't need to write return keyword
+//this tecknique will very usefull in react
+
+//when you need to use object. then the object must be inside curly braces but the curly braces must be inside parenthesis
+const addObject = () => ({username: "Bikram"})
+// console.log(addObject());
+
+
