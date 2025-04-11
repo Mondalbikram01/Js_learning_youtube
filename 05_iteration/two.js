a21c5e84b98abd69941ad1b7d160eb7cb70e6f89
Bikram Mondal <bikrammondalofficial1@gmail.com>
2025-04-11 10:00:29 +0000

    array with

---
+++ 05_iteration/two.js
@@ -0,0 +1,13 @@
+let index = 0 //veriable declared
+while (index <= 10) {//condition inside parenthesis
+    // console.log(`value of index is ${index}`);
+    index = index + 2//increment by two // it is necessary to stop the loop or the loop will go infinite
+}
+
+
+let myArray = [ "superman", "batman", "spiderman"]
+let arr = 0
+while (arr < myArray.length) {
+    console.log(`hero name ${myArray[arr]}`)   
+    arr = arr + 1
+}
